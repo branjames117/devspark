802ae420c82580c02df021e6b00c6187a362b8bb
cody-junier <codyjunier@gmail.com>
2022-01-20 20:23:52 -0800

    Changing functionality to display starred github repos

--- public/js/gitIssues.js
+++ public/js/gitIssues.js
@@ -2,7 +2,7 @@
 async function githubIssues(user){
     // user will be userID for github
     const user = document.querySelector("#githubusername").value;
-    const apiUrl = "https://api.github.com/users/" + user +"/repos";
+    const apiUrl = "https://api.github.com/users/" + user +"/starred";
     // grab users public github repo data
     fetch(apiUrl).then(function(response){
         if(response.ok){
@@ -13,7 +13,26 @@ async function githubIssues(user){
                     for (let i = 0; i < repos.length; i++) {
                         const repoName = repos[i].name;
 
-                        
+                        var repoEl = document.createElement("a");
+                        repoEl.classList= "list-item flex-row justify-space-between align-center";
+                        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+
+                        var titleEl= document.createElement("span");
+                        titleEl.textContent= repoName;
+
+                        repoEl.appendChild(titleEl);
+
+                        var statusEl = document.createElement("span");
+                        statusEl.classList="flex-row align-center";
+
+                        if (repos[i].open_issues_count > 0){
+                            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" +repos[i].open_issues_count + " issue(s)";
+
+                        }else{
+                            statusEl.innerHTML= "<i class='fas fa-check-square status-icon icon-success'></i>";
+                        }
+                        repoEl.appendChild(statusEl);
+                        repoContainerEl.appendChild(repoEl);
                     }
                 };
             })
