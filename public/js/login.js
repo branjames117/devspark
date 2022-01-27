b65046662b56761f6c88e749568619a27e35ac07
Cody-Junier <90635812+Cody-Junier@users.noreply.github.com>
2022-01-27 15:57:16 -0800

    Update login.js
    now leading to chats, not profile

--- public/js/login.js
+++ public/js/login.js
@@ -37,7 +37,7 @@ async function loginFormHandler(event) {
       headers: { 'Content-Type': 'application/json' },
     });
     if (response.ok) {
-      document.location.replace(`/`);
+      document.location.replace(`/chats`);
     } else {
       errMessageEl.innerHTML =
         'Something went wrong. Are you sure the username and password are correct?<br />';
