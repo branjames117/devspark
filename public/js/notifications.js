38069a15af1495a8c6bcf175da9558f23620322d
Brandon Hoskins <branjames117@gmail.com>
2022-01-27 20:48:26 -0600

    closing issues 67, 68

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -1,6 +1,10 @@
 var socket = io();
 
+const notifSound = new Audio('/sounds/msg_sent.wav');
+
 socket.emit('open-notifications');
 socket.on('update-notifications', (data) => {
+  const soundEnabled = localStorage.getItem('soundEnabled');
+  if (soundEnabled == 1) notifSound.play();
   document.getElementById('notif-count').textContent = data;
 });
